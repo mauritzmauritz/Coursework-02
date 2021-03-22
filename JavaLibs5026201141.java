921e83ab4133e96f73a9ada11ea7184531071c7f
mauritzmauritz <81178116+mauritzmauritz@users.noreply.github.com>
2021-03-22 23:19:49 +0700

    Update JavaLibs5026201141.java

--- JavaLibs5026201141.java
+++ JavaLibs5026201141.java
@@ -2,14 +2,14 @@ import javax.swing.JOptionPane;
 
 public class JavaLibs5026201141 {
 	public static void main(String[] args) {
-		JOptionPane.showMessageDialog("Hey, I do not think I have met you before.");
+		JOptionPane.showMessageDialog(null, "Hey, I do not think I have met you before.");
 		String name = JOptionPane.showInputDialog("What do I call you?");
-		JOptionPane.showMessageDialog("Nice to meet you, " + name + " !");
+		JOptionPane.showMessageDialog(null, "Nice to meet you, " + name + " !");
 		int age = Integer.parseInt(JOptionPane.showInputDialog("How old are you?"));
 		String birth = JOptionPane.showInputDialog("Where were you born?");
 		String hobby = JOptionPane.showInputDialog("What do you usually do to spend your time?");
 		String school = JOptionPane.showInputDialog("Hey, I sometimes do " + hobby + " too! Where do you go to school?");
-		int pn = Integer.parseInt(JOptionPane.showInputDialog("May I have your phone number?"));
+		int pn = Integer.parseInt(JOptionPane.showInputDialog("May I have your last 4 digits of phone number?"));
 		String ffood = JOptionPane.showInputDialog("By the way, what's your favorite food?");
 		double sumfood = Double.parseDouble(JOptionPane.showInputDialog("How many " + ffood + " do you eat in one sitting?"));
 
@@ -17,10 +17,7 @@ public class JavaLibs5026201141 {
 		double selffood = sumfood + 1.5;
 		JOptionPane.showMessageDialog(
 			null,
-			"Hello, " + name + "! I am Mauritz. I am about " + selfage + " years old. I was born in Surabaya,
-			but I am sure I will put " + birth + " to my bucket list! When I go there, we can do " +
-			hobby + " together, and maybe even go to " + school + " to meet your teachers! After the
-			pandemic is over, I guess. Well, I'll be sure to put " + pn + " to my contacts list, and maybe we could even cook " + selffood + " more " + ffood + " than you usually do! See you later!", 
-			0);
+			"Hello, " + name + "! I am Mauritz. I am about " + selfage + " years old.\n I was born in Surabaya, but I am sure I will put " + birth + " to my bucket list! When I go there, we can do " +
+			hobby + " together, and maybe even go to " + school + " to meet your teachers!\n After the pandemic is over, I guess. Well, I will  be sure to put xxxxxxx" + pn + " to my contacts list, and maybe we could even cook " + selffood + " more " + ffood + " than you usually do! See you later!");
 }
 }
